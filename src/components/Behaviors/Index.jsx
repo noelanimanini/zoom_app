ec8b68b77a75bedda7e3c6f797d04176cacd315a
crystal <noelani.villanueva@gmail.com>
2021-06-19 19:25:05 -0700

    meetings done

--- src/components/Behaviors/Index.jsx
+++ src/components/Behaviors/Index.jsx
@@ -1,24 +1,34 @@
 import React from "react";
-import Pic from "../images/inclusive.png";
+import Pic from "../images/inclusive.jpg";
 import "./behaviors.css";
 function Index() {
   return (
     <div className="message">
+      <div className="titleBox">
+        <h1 className="title">
+          Digital Accessibility & Inclusion are <em> Essential</em>
+        </h1>
+      </div>
+
       <div className="behavior">
         <img src={Pic} className="behaviorImage" />
-        <div className="box">
-          <h2 className="boxhead">
-            bringing welcoming, empathetic, & open minds listening to all
-            opinions ensuring mutual understanding uplifting voices that may not
-            always be heard
+        <div>
+          <h2 id="boxhead">
+            <p id="boxhead2">
+              From school, work, to casual socialization, video conferencing has
+              been an efficient way to connect and share information, especially
+              since the COVID-19 shutdown.
+            </p>
+            <p id="boxhead2">
+              However, video conferencing creates many accessibility barriers,
+              meaning not all communities can participate equally.
+            </p>
+            Digi-Clusive Meeting intends to remove those barriers, because,{" "}
+            <strong>
+              {" "}
+              in order to have true community, every voice is necessary.
+            </strong>
           </h2>
-          <ul className="ulDecoration">
-            <h3>
-              <li>lol</li>
-              <li>lol</li>
-              <li>lol</li>
-            </h3>
-          </ul>
         </div>
       </div>
     </div>
--- src/components/Meetings/Index.jsx
+++ src/components/Meetings/Index.jsx
@@ -1,25 +1,64 @@
 import React from "react";
-import computer from "../images/computer.png";
+import computer from "../images/computer4.jpg";
 import "./meetings.css";
 
 function Index() {
   return (
-    <div className="parentBlurb">
-      <div className="blurb">
-        <h1>DIGI-CLUSIVE MEETINGS</h1>
+    <div>
+      <div className="parentBlurb">
+        <div className="subParent">
+          <h1 className="titleTag"> DIGI-CLUSIVE MEETINGS</h1>
+          <div className="paraParent">
+            <div className="para1">
+              <p>
+                Digi-Clusive is an easily downloadable chatbot for Zoom to help
+                make digital meet-ups more welcoming for people with a variety
+                of accessibility needs. <br />
+              </p>
+            </div>
+            <div className="para2">
+              <p>
+                {" "}
+                To encourage digital inclusion, the bot sends automatic
+                reminders to:
+                <ul>
+                  <li>mute the mic when not speaking</li>
+                  <li>turn on closed captioning</li>
+                  <li>include image descriptions</li>
+                  <li>and much more...</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="imageParent">
+          <img src={computer} className="imageComputer" />
+        </div>
+      </div>
+
+      {/* <div className="blurb">
         <p>
-          Digi-Clusive is an easily downloadable chat bot for zoom to help make
+          Digi-Clusive is an easily downloadable chatbot for Zoom to help make
           digital meet-ups more welcoming for people with a variety of
-          accessibility needs. To encourage natural inclusivity, the bot sends
-          automatic reminders to: mute the mic when not speaking turn on closed
-          captioning include image descriptions and much more...
+          accessibility needs. <br />
+        </p>
+
+        <p>
+          {" "}
+          To encourage digital inclusion, the bot sends automatic reminders to:
+          <ul>
+            <li>mute the mic when not speaking</li>
+            <li>turn on closed captioning</li>
+            <li>include image descriptions</li>
+            <li>and much more...</li>
+          </ul>
         </p>
       </div>
       <div className="parentDiv">
         <div>
           <img src={computer} className="imageComputer" />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
